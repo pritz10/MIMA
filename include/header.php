26bd97903a899448cfa4b8358b49459171c91381
Pritz <pritzzzzz101010@gmail.com>
2023-11-20 12:38:15 +0530

    etgeg

--- include/header.php
+++ include/header.php
@@ -44,7 +44,7 @@ require_once 'include/db.php';?>
     </head>
    <body>
       <div class="header-outs" id="home">
-             <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background: black;">
+             <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background: black; padding:0;">
                <a class="navbar-brand" href="index.html"><img src="files/Images/log.PNG" width="100"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
@@ -52,7 +52,7 @@ require_once 'include/db.php';?>
                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                   <ul class="navbar-nav ">
                      <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" style="font-weight: 900;" href="index.php">Home <span class="sr-only">(current)</span></a>
                      </li>
                      <li class="nav-item">
                         <a href="#" class="nav-link">About</a>
