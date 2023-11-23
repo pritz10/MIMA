61ab4177c5500d8ce1dba548cc1ef0e8ff87cbcf
Pritz <pritzzzzz101010@gmail.com>
2023-11-23 22:55:09 +0530

    Update db.php

--- pritz_e_nIN1_81en_IN8_21_PS_Ds_809_12/db.php
+++ pritz_e_nIN1_81en_IN8_21_PS_Ds_809_12/db.php
@@ -1,15 +1,15 @@
 <?php 	
 $localhost = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mim";
+$username = "u623909803_pritz";
+$password = "Pritz@100";
+$dbname = "u623909803_mim";
  // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
 if($connect->connect_error) {
   die("Connection Failed : " . $connect->connect_error);
   echo "Not  connected";
 } else {
-
   //echo "Successfully connected";
  }
 ?>
+
