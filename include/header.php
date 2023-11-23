373c6fafb6cb44ce47c5ef12e6b405f214962c30
Pritz <pritzzzzz101010@gmail.com>
2023-11-23 12:49:30 +0530

    Update header.php

--- include/header.php
+++ include/header.php
@@ -7,13 +7,13 @@ header('set Referrer-Policy same-origin');
 require_once 'include/db.php';?>
  
 <!DOCTYPE html>
-<html lang="zxx">
-   <head>
-      <title>Music In Motion Dance Academy | Mim dance academy sikkim</title>
-      <!--meta tags -->
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<html lang="en">
+<head>
+<title>Music In Motion Dance Academy | Mim dance academy sikkim</title>
+<!--meta tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
 <meta http-equiv="Pragma" content="no-cache" />
 <meta http-equiv="Expires" content="0" />
       <meta name="keywords" content="MIMA, mima, Music in motion dance academy, dance academy in sikkim, sikkim, gangtok , mim dance academy" />
