67a19f7bdbc57958e39051ef7543c1854a4fc9d6
Pritz <pritzzzzz101010@gmail.com>
2023-11-25 16:57:39 +0530

    Update about.php

--- about.php
+++ about.php
@@ -175,7 +175,7 @@ The vision of the academy has been to offer platforms to aspiring and deserving 
                <div class="col-lg-4 col-md-4 abut-inner-img">
                <img src="files/Images/rumba_6901480.png" width="200px" alt="" class="img-fluid">
                   <div class="w3l-abt-sub-txt mt-lg-4 mt-3">
-                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+                  <p style="color: wheat;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
                      <h4 class="mt-3">Balinese</h4>
                   </div>
                </div>
