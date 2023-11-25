f5c650d953c8cf6fe759aa7e25bff3a85f75235d
Pritz <pritzzzzz101010@gmail.com>
2023-11-25 16:45:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -407,7 +407,8 @@ Our goal is to assist you in creating the best version of yourself and developin
       <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
     </div>
 
-       <div class="card-deck mb-3 text-center">
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
           <div class="card-header">
             <h4 class="my-0 font-weight-normal">Free</h4>
@@ -453,7 +454,7 @@ Our goal is to assist you in creating the best version of yourself and developin
             <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
           </div>
         </div>
-      </div>
+        </div>
 
 
 
