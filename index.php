8e817cd6a51f282a2df84540c0b3ef1eb1013ef1
Pritz <pritzzzzz101010@gmail.com>
2023-11-25 16:48:17 +0530

    adcfsd

--- index.php
+++ index.php
@@ -401,7 +401,7 @@ Our goal is to assist you in creating the best version of yourself and developin
 
 
 
-
+      <section style="background-color: black;">
       <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
       <h1 class="display-4">Pricing</h1>
       <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
@@ -469,7 +469,7 @@ Our goal is to assist you in creating the best version of yourself and developin
           </div>
         </div>
         </div>  </div>
-
+</section>
 
 
 
