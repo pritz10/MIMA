85b28b4fc75f4cca764d089b5951cec9978f5a84
Pritz <pritzzzzz101010@gmail.com>
2023-11-26 18:14:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -409,7 +409,7 @@ Our goal is to assist you in creating the best version of yourself and developin
       <h3 class="title text-left clr mb-lg-5 mb-md-4 mb-sm-4 mb-3">Fee Structure</h3>
       <p class="lead">Choose the best subscription for you. One time Admission Rs 3000/-</p>
     </div>
-      <div class="card-deck mb-3 text-center">
+      <div class="card-deck text-center">
       <div class="card mb-4 box-shadow">
           <div class="card-header">
             <h4 class="my-0 font-weight-normal"> <strong>Monthly</strong></h4>
