0ddce01062e37d888f5be0f6bdeabe10ad00b248
Pritz <pritzzzzz101010@gmail.com>
2023-11-26 18:15:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -477,7 +477,7 @@ Our goal is to assist you in creating the best version of yourself and developin
 </section>      <!--testimonial-->
 <section class="testimonial py-lg-4 py-md-3 py-sm-3 py-3">
          <div class="container py-lg-5 py-md-5 py-sm-4 py-3">
-            <h3 class="title text-left mb-lg-5 mb-md-4 mb-sm-4 mb-3">Our Dancers Says</h3>
+            <h3 class="title text-left mb-lg-5 mb-md-4 mb-sm-4 mb-3" style="color: white;">Our Dancers Says</h3>
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                <div class="carousel-inner text-center" >
               
