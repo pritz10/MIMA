4537039fa586f5495bd28e9a1136a37752765feb
Pritz <pritzzzzz101010@gmail.com>
2023-11-26 20:11:37 +0530

    Update class.php

--- class.php
+++ class.php
@@ -69,6 +69,81 @@ require_once 'include/header.php'; ?>
          </div>
       </section>
       <section>
+
+      <section style="background-color: black;">
+     
+
+    <div class="container">
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h3 class="title text-left clr mb-lg-5 mb-md-4 mb-sm-4 mb-3">Fee Structure</h3>
+      <p class="lead">Choose the best subscription for you. One time Admission Rs 3000/-</p>
+    </div>
+      <div class="card-deck text-center">
+      <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal"> <strong>Monthly</strong></h4>
+          </div>
+          <div class="card-body" style="background: #fb6509;">
+          <h1 class="card-title pricing-card-title">₹3k</h1>
+
+          <ul class="list-unstyled mt-3 mb-4" style="color: white; font-weight: 500;">
+              <li>Saturday-Sunday</li>
+              <li>2 Days in a Week</li>
+              <li>8 Days in a Month</li>
+              
+
+
+
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-light">Sign up for free</button>
+        </div>
+        </div> <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal"><Strong>Quarterly</Strong></h4>
+          </div>
+          <div class="card-body" style="background: #fb6509;">
+          <h1 class="card-title pricing-card-title">₹6000/-</h1>
+
+          <ul class="list-unstyled mt-3 mb-4" style="color: white; font-weight: 500;">
+            <li>Saturday-Sunday</li>
+            <li>2 Days in a Week</li>
+            <li>8 Days in a Month</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-warning">Sign up for free</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+          <h4 class="my-0 font-weight-normal"><Strong>Half-Yearly</Strong></h4>
+          </div>
+          <div class="card-body" style="background: #fb6509;">
+          <h1 class="card-title pricing-card-title"><strike>₹12</strike>11k</h1>
+
+          <ul class="list-unstyled mt-3 mb-4" style="color: white; font-weight: 500;">
+            <li>Saturday-Sunday</li>
+            <li>2 Days in a Week</li>
+            <li>8 Days in a Month</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-warning">Sign up for free</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+          <h4 class="my-0 font-weight-normal"><Strong>Annualy</Strong></h4>
+          </div>
+          <div class="card-body" style="background: #fb6509;">
+          <h1 class="card-title pricing-card-title"><strike>₹24.5</strike>23k</h1>
+
+            <ul class="list-unstyled mt-3 mb-4" style="color: white; font-weight: 500;">
+            <li>Saturday-Sunday</li>
+            <li>2 Days in a Week</li>
+            <li>8 Days in a Month</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-warning">Talk to Us</button>
+          </div>
+        </div>
+        </div>  </div>
+</section>    
          <div class="container-fluid text-center">
             <div class="row abt-inner-agile">
                <div class="col-lg-6 col-md-6 two-abut-inner-right pr-0">
