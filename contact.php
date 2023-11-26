50786d391ab51f58543216ddfcfca74b48f2a9fb
Pritz <pritzzzzz101010@gmail.com>
2023-11-26 20:52:37 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -64,8 +64,8 @@ require_once 'include/header.php'; ?>
                         </p>
                      </div>
                   </div>
-                  <div class="address_mail_footer_grids pt-lg-4 pt-3">
-                     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3539.812628729253!2d153.014155!3d-27.4750921!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b915a0835840a2f%3A0xdd5e3f5c208dc0e1!2sMelbourne+St%2C+South+Brisbane+QLD+4101%2C+Australia!5e0!3m2!1sen!2sin!4v1492257477691"></iframe>
+                   <div class="address_mail_footer_grids pt-lg-4 pt-3">
+                     <iframe src="https://www.google.com/maps/embed/v1/place?q=MUSIC+IN+MOTION+DANCE+ACADEMY,+Mahatma+Gandhi+Marg,+near+Sass+&+Guff+Restaurant,+Vishal+Gaon,+Gangtok,+Sikkim,+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
                   </div>
                </div>
             </div>
