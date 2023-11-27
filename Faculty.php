9f8e8b67fc9855c205da766bfc51f2b775a7ace1
Pritz <pritzzzzz101010@gmail.com>
2023-11-27 08:42:55 +0530

    Create Faculty.php

---
+++ Faculty.php
@@ -0,0 +1,161 @@
+<?php
+$title = "About Us";
+require_once 'include/header.php'; ?>
+
+      <div class="inner_page-banner">
+      </div>
+      <!--//banner -->
+      <!-- short -->
+      <div class="using-border py-3">
+         <div class="inner_breadcrumb  ml-4">
+            <ul class="short_ls">
+               <li style="color:blanchedalmond">
+                  <a href="index.php">Home</a>
+                  <span>/</span>
+               </li>
+               <li style="color:blanchedalmond">About</li>
+            </ul>
+         </div>
+      </div>
+      <!-- //short-->
+      <!--about-->
+      <section class="about-inner py-lg-4 py-md-3 py-sm-3 py-3">
+         <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+            <h3 class="title text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">About Us</h3>
+            <div class="section s1">
+
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Aakash<br>
+    <span>Urban Choreography & Old School </span></h1>
+  <p>&nbsp;</p>
+  <p></p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/bc1qre8jdw2azrg6tf49wmp652w00xltddxmpk98xp.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Ashna<br>
+    <span>Contemporary</span></h1>
+  <p>&nbsp;</p>
+  <p></p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/414ce6735e34ab731ed5a1330c3011b8.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Atul & Karan<br>
+    <span>Contemporaray (Advanced Beginner / Intermediate batch)</span></h1>
+  <p>&nbsp;</p>
+  <p>Atul Jindal is a dancer/teacher/choreographer who is constantly looking for inspiration to create new work. He is very particular about his continual personal and creative growth and is passionate about bringing his knowledge of choreography, storytelling, direction, and life perspective to the stage.</p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/bc1qre8jdw2azrg6tf49wmp652w00xltddxmpk98xp.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>B-Boy Makhan<br>
+    <span>Breaking</span></h1>
+  <p>&nbsp;</p>
+  <p></p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/906b507757ad6362a99538988b70d0e8.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Dheeraj Soni<br>
+    <span>Old School / House / Locking</span></h1>
+  <p>&nbsp;</p>
+  <p>Dheeraj soni is an ardent mover, versatile dancer and a house dance specialist. Trained in hip hop, street and contemporary styles of dance,dheeraj is known for his free-styling, improvising and precision. His urge to learn and explore endlessly makes him a true artist. He is a recent addition to the Big Dance faculty.</p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/b97f0f2b53c22906c9c68350ecf808b0.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Gaurav & Ankit <br>
+    <span>Urban Choreography </span></h1>
+  <p>&nbsp;</p>
+  <p>They are tight, indispensible and they are here to prove that strong determination with equal amount of hard work can truly transform dust to gold. Ankit Gaurav are the finest duo we have in our family and the family is proud of it. </p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/804748b2716a31115c316df637a41c8a.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Karan Kumar <br>
+    <span>Ballet & Contemporary dancer</span></h1>
+  <p>&nbsp;</p>
+  <p>Karan Kumar is a dancer who has succeeded in creating his own niche in India. Having studied in one of the finest schools in New York, he has a natural flair for dance, a fact that has been accredited by many in the Indian industry.</p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/c6d2c9802b245ae4a0f60dc7cdcc356e.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Kunal<br>
+    <span>Urban Choreography </span></h1>
+  <p>&nbsp;</p>
+  <p>As Kunal describes it, being passionate while dancing and transmitting feelings makes a dance performance enjoyable and relatable for the viewer. Kunal is a young, honest and rooted individual who is </p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/741b426482ed42a8e0000ca86256bfc4.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Mohit <br>
+    <span>Urban Choreography </span></h1>
+  <p>&nbsp;</p>
+  <p>No Words, ALL Do. Mohit is a silent storm who believes in delivering more than dissecting. This powerful dancer comes from the most humble backgrounds and truly defines an artist of excellence that he is.</p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/a86b638a86c91b371e4742a26e365d90.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Navya<br>
+    <span>Hip-Hop</span></h1>
+  <p>&nbsp;</p>
+  <p></p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/f5e439cb6d7117c147a51135b081a591.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+<section>
+<article class="pdsp fl wow fadeInLeft" data-wow-delay="0.4s">
+  <h1>Shubham<br>
+    <span>Contemporary</span></h1>
+  <p>&nbsp;</p>
+  <p>This young versatile and passionate artist makes our family nothing but stronger. Shubham is determined to create a niche for himself with his intricate and interesting movement that originates from all that he has attained over the years as a dancer and performer. </p>
+ 
+</article>
+<article class="pdspImg fr  wow fadeInRight" data-wow-delay="0.5s"><div class="imgresize" style="margin-bottom:3%"><img src="assets/adminhtml/img/pages/faculty/cbcecef7bbc4ef5f4dcb6185723e64a9.jpg" alt="" style="width: 346px; height: auto;"></div>
+</article>
+<div class="cb"></div>
+</section>
+</div>
+         </div>
+      <?php require_once 'include/footer.php';?>
