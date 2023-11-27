3464720d18eb5af752524b3eae0ef86cd21b1b5d
Pritz <pritzzzzz101010@gmail.com>
2023-11-27 11:59:30 +0530

    Update class.php

--- class.php
+++ class.php
@@ -108,7 +108,7 @@ require_once 'include/header.php'; ?>
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLong">Hip-Hop</h5>
+        <h5 class="modal-title" id="exampleModalLong">All style dance training</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
