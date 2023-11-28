50b5e9de771744feb1f2d2ea4efd9e548cb9b5a2
Pritz <pritzzzzz101010@gmail.com>
2023-11-28 12:14:54 +0530

    Update header.php

--- include/header.php
+++ include/header.php
@@ -73,16 +73,18 @@ require_once 'include/db.php';?>
                         <a href="about" class="nav-link">About</a>
                      </li>
                      <li class="nav-item">
+                     <a href="#" class="nav-link">Store</a>
+                     </li>
+                     <li class="nav-item">
                      <a href="#" class="nav-link">Gallery</a>
                      </li>
                      <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown
+    Dance Camps
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <a href="" class="dropdown-item" type="button">Action</a>
-    <a href="" class="dropdown-item" type="button">Another action</a>
-    <a href="" class="dropdown-item" type="button">Something else here</a>
+     <a href="#" class="dropdown-item" type="button">Winter Camp</a>
+    <a href="#" class="dropdown-item" type="button">Summer Camp</a>
   </div>
 </div>
                      <li class="nav-item">
