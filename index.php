5a78ecb91e818560be8f6d5431bd2c600c02174f
Pritz <pritzzzzz101010@gmail.com>
2023-12-01 23:17:04 +0530

    ewew

--- index.php
+++ index.php
@@ -506,11 +506,11 @@ Our goal is to assist you in creating the best version of yourself and developin
       <div class="card-deck text-center">
       <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal"> <strong>Monthly All Style Dance Style</strong></h4>
+            <h4 class="my-0 font-weight-normal"> <strong>Monthly <br>All Style Dance Style</strong></h4>
           </div>
           <div class="card-body" style="background: #fb6509;">
-          <h1 class="card-title">₹ 2k Monthly <br>
-₹ 3k One Time Registration </h1>
+          <p class="card-title">₹ 2k Monthly <br>
+₹ 3k One Time Registration </p>
 
           <ul class="list-unstyled mt-3 mb-4" style="color: white; font-weight: 500;">
               <li>Saturday-Sunday</li>
