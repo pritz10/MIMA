dadfa847ae6577b8c79e4eea09ab822d1ba2fc5f
Pritz <pritzzzzz101010@gmail.com>
2023-12-01 23:15:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -502,15 +502,15 @@ Our goal is to assist you in creating the best version of yourself and developin
     <div class="container">
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
       <h3 class="title text-left clr mb-lg-5 mb-md-4 mb-sm-4 mb-3">Fee Structure</h3>
-      <p class="lead">Choose the best subscription for you. One time Admission Rs 3000/-</p>
-    </div>
+     </div>
       <div class="card-deck text-center">
       <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal"> <strong>Monthly</strong></h4>
+            <h4 class="my-0 font-weight-normal"> <strong>Monthly All Style Dance Style</strong></h4>
           </div>
           <div class="card-body" style="background: #fb6509;">
-          <h1 class="card-title pricing-card-title">₹3k</h1>
+          <h1 class="card-title pricing-card-title">₹ 2k Monthly <br>
+₹ 3k One Time Registration </h1>
 
           <ul class="list-unstyled mt-3 mb-4" style="color: white; font-weight: 500;">
               <li>Saturday-Sunday</li>
