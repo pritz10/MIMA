8559a43df1d4abe09f5c7c714325fee9f27f8282
Pritz <pritzzzzz101010@gmail.com>
2023-12-02 13:36:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -509,7 +509,7 @@ Our goal is to assist you in creating the best version of yourself and developin
       <div class="card-deck text-center">
       <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal"> <strong>Monthly <br>All Style Dance Style</strong></h4>
+            <h4 class="my-0 font-weight-normal"> <strong>Monthly <br>All Style Dance Class</strong></h4>
           </div>
           <div class="card-body" style="background: #fb6509;">
           <p class="card-title">₹ 2k Monthly <br>
@@ -548,7 +548,7 @@ Our goal is to assist you in creating the best version of yourself and developin
         </div>
         <div class="card mb-4 box-shadow">
           <div class="card-header">
-            <h4 class="my-0 font-weight-normal"> <strong>Monthly <br>Gymnastics Class Style</strong></h4>
+            <h4 class="my-0 font-weight-normal"> <strong>Monthly <br>Gymnastics Class</strong></h4>
           </div>
           <div class="card-body" style="background: #fb6509;">
           <p class="card-title">₹2k Monthly <br>
