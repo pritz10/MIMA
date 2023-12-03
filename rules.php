0a42e240beac6bf36830189165368b28f7528605
Pritz <pritzzzzz101010@gmail.com>
2023-12-03 16:58:49 +0530

    wefwef

---
+++ rules.php
@@ -0,0 +1,33 @@
+<?php
+$title = "Rules";
+require_once 'include/header.php'; ?>
+
+      <div class="inner_page-banner">
+      </div>
+      <!--//banner -->
+      <!-- short -->
+      <div class="using-border py-3">
+         <div class="inner_breadcrumb  ml-4">
+            <ul class="short_ls">
+               <li style="color:blanchedalmond">
+                  <a href="index.php">Home</a>
+                  <span>/</span>
+               </li>
+               <li style="color:blanchedalmond">Sikkim Big Dance Camp</li>
+            </ul>
+         </div>
+      </div>
+    <div class="container">
+<p>
+   <strong>
+   FEE REGULATION  </strong>
+1. Tuition fees should be paid for 12 months irrespective of the date of joining the academy.  <br>
+2. Two months dance  fee  should pay in advance. <br>
+3. Tuition fees must be paid before the  1st to 10th of every first month. <br>
+4. The Registration fee and the admission fee is not refundable. <br>
+5. Any student discontinuing the course must pay the fee for the particular month. <br>
+6.All registered student of the academy are required to renew their registration each year. Renewal Fee ₹1500
+ 
+</p>
+    </div>
+      <?php require_once 'include/footer.php';?>
