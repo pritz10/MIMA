e20430333476c9181d48c30ab07cb4b8da17bd02
Pritz <pritzzzzz101010@gmail.com>
2023-12-03 17:00:19 +0530

    Update rules.php

--- rules.php
+++ rules.php
@@ -16,11 +16,11 @@ require_once 'include/header.php'; ?>
                <li style="color:blanchedalmond">Sikkim Big Dance Camp</li>
             </ul>
          </div>
-      </div>
+      </div> <br>
     <div class="container">
 <p>
    <strong>
-   FEE REGULATION  </strong>
+   FEE REGULATION  </strong> <br>
 1. Tuition fees should be paid for 12 months irrespective of the date of joining the academy.  <br>
 2. Two months dance  fee  should pay in advance. <br>
 3. Tuition fees must be paid before the  1st to 10th of every first month. <br>
@@ -28,6 +28,16 @@ require_once 'include/header.php'; ?>
 5. Any student discontinuing the course must pay the fee for the particular month. <br>
 6.All registered student of the academy are required to renew their registration each year. Renewal Fee ₹1500
  
+
+<br> <br>
+<br>
+<strong>GENERAL INSTRUCTION</strong><br>
+1. The fee book and the Identity Card must be preserved carefully. In cascofloss, a duplicate card will be issued on payment of Rs. 100 with DUPLICATE Copy.
+<br> 2. Attendance in tutorials or special classes is compulsory. <br>
+3. Teacher-Student-Guardians' meeting, conducted by the academy should be attended. <br>
+4. Interaction of the students and teachers it. <br>
+The administration of the academy patronizes the student feedback system for quality control in imparting education.
+5. Don't come late and if you do enter very quietly <br>
 </p>
     </div>
       <?php require_once 'include/footer.php';?>
