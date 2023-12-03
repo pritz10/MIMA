a910ac0c612dbdaac0a1bc7363e63336ae0203e3
Pritz <pritzzzzz101010@gmail.com>
2023-12-03 09:54:57 +0530

    Update store.php

--- store.php
+++ store.php
@@ -69,6 +69,53 @@ require_once 'include/header.php'; ?>
                 </div>
               </div>
             </div>
+
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+              <img class="card-img-top img-fluid" src="files/Images/s.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">
+                  This Tshirt is made of high-quality materials that will keep you sweat free and comfortable all in all dance season 
+                </p>
+              
+              <br> Availabe in all sizes:    <div class="btn-group">
+ 
+ <a href="" class="btn btn-sm btn-outline-secondary">XS</a>
+ <a href="" class="btn btn-sm btn-outline-secondary">S</span></a>
+ <a href="" class="btn btn-sm btn-outline-secondary">M</a>
+ <a href="" class="btn btn-sm btn-outline-secondary">L</span></a>
+ <a href="" class="btn btn-sm btn-outline-secondary">XL</a>
+ <a href="" class="btn btn-sm btn-outline-secondary">XXL</span></a>
+</div>
+                  <div class="outs-agile-buttn mt-lg-3 mt-2">
+                  <a href="https://api.whatsapp.com/send?phone=+918918212479"  style="font-weight: bolder;">Whatsapp Us</a>
+                     </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+              <img class="card-img-top img-fluid" src="files/Images/t.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">
+                  This Tshirt is made of high-quality materials that will keep you sweat free and comfortable all in all dance season 
+                </p>
+              
+              <br> Availabe in all sizes:    <div class="btn-group">
+ 
+ <a href="" class="btn btn-sm btn-outline-secondary">XS</a>
+ <a href="" class="btn btn-sm btn-outline-secondary">S</span></a>
+ <a href="" class="btn btn-sm btn-outline-secondary">M</a>
+ <a href="" class="btn btn-sm btn-outline-secondary">L</span></a>
+ <a href="" class="btn btn-sm btn-outline-secondary">XL</a>
+ <a href="" class="btn btn-sm btn-outline-secondary">XXL</span></a>
+</div>
+                  <div class="outs-agile-buttn mt-lg-3 mt-2">
+                  <a href="https://api.whatsapp.com/send?phone=+918918212479"  style="font-weight: bolder;">Whatsapp Us</a>
+                     </div>
+                </div>
+              </div>
+            </div>
             
           </div>
           </div>
