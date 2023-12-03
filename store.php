5f9091f3ee6cdf70f3b4bbcda972809db11ec571
Pritz <pritzzzzz101010@gmail.com>
2023-12-03 14:25:20 +0530

    Update store.php

--- store.php
+++ store.php
@@ -26,13 +26,9 @@ require_once 'include/header.php'; ?>
                 <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
                 <div class="card-body">
                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
+                  <div class="outs-agile-buttn mt-lg-3 mt-2">
+                  <a href="https://api.whatsapp.com/send?phone=+918918212479"  style="font-weight: bolder;"><img src="files/Images/w.png" class="img-fluid" alt="Cal Us"></a>
+                     </div>
                 </div>
               </div>
             </div>
